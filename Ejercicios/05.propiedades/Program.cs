01604bf0718fb10f0787aeea7638c33d6225b72b
IrisVH <ivhernandez@unah.hn>
2020-10-26 17:41:44 -0600

    agregando ejercicios hasta el 06

---
+++ Ejercicios/05.propiedades/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.PrimerNombre= "Juan";
+            a.SegundoNombre ="Mario";
+           
+
+            Alumno b = new Alumno();
+            b.Id=2;
+            b.PrimerNombre="Maria";
+            b.SegundoNombre="Carmen";
+
+            Alumno c = new Alumno(3);
+            c.PrimerNombre= "Pedro";
+
+            Alumno d = new Alumno("Juan", "Jimenez");
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " "+ d.SegundoNombre);
+
+        }
+    }
+}
